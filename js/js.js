98110b29992fe8a7cc8804ffbd0c1d23cc83b0ab
Karol Kokosza <karol@kokosza.com.pl>
2022-06-19 12:54:28 +0200

    changes

--- js/js.js
+++ js/js.js
@@ -12,10 +12,16 @@ function resize(){
 
 function sizeUp(){
     header.style = "clip-path: circle(10.9% at 50% 50%);"
+    header.querySelector(".logo-small").style = "opacity: 1;"
+    header.querySelector(".heading").style = "opacity: 0;"
+    header.querySelector(".logo").style = "opacity: 0;"
 }
 
 function sizeDown(){
     header.style = "clip-path: circle(114% at 50% 50%);"
+    header.querySelector(".logo-small").style = "opacity: 0;"
+    header.querySelector(".heading").style = "opacity: 1;"
+    header.querySelector(".logo").style = "opacity: 1;"
 }
 
 window.addEventListener("resize", resize)
@@ -41,10 +47,33 @@ sect1.addEventListener("click", function(){
     sect1.querySelector(".bg").classList.add("bg-active")
     sect1.querySelector(".content").classList.add("bg-active")
     header.classList.add("sect-active")
+    header.querySelector(".logo-small").style = "opacity: 1;"
+    header.querySelector(".heading").style = "opacity: 0;"
+    header.querySelector(".logo").style = "opacity: 0;"
+    sect1.removeEventListener("mouseover", sizeUp)
+    sect1.removeEventListener("mouseout", sizeDown)
+    sect2.removeEventListener("mouseover", sizeUp)
+    sect2.removeEventListener("mouseout", sizeDown)
+    sect3.removeEventListener("mouseover", sizeUp)
+    sect3.removeEventListener("mouseout", sizeDown)
+    sect4.removeEventListener("mouseover", sizeUp)
+    sect4.removeEventListener("mouseout", sizeDown)
     setTimeout(function(){
         sect1.querySelector(".bg").classList.remove("bg-active")
         sect1.querySelector(".content").classList.remove("bg-active")
         header.classList.remove("sect-active")
+        header.querySelector(".logo-small").style = "opacity: 0;"
+        header.querySelector(".heading").style = "opacity: 1;"
+        header.querySelector(".logo").style = "opacity: 1;"
+        header.style = "clip-path: circle(114% at 50% 50%);"
+        sect1.addEventListener("mouseover", sizeUp)
+        sect1.addEventListener("mouseout", sizeDown)
+        sect2.addEventListener("mouseover", sizeUp)
+        sect2.addEventListener("mouseout", sizeDown)
+        sect3.addEventListener("mouseover", sizeUp)
+        sect3.addEventListener("mouseout", sizeDown)
+        sect4.addEventListener("mouseover", sizeUp)
+        sect4.addEventListener("mouseout", sizeDown)
     }, 6000)
 })
 
@@ -52,33 +81,102 @@ sect2.addEventListener("click", function(){
     sect2.querySelector(".bg").classList.add("bg-active")
     sect2.querySelector(".content").classList.add("bg-active")
     header.classList.add("sect-active")
+    header.querySelector(".logo-small").style = "opacity: 1;"
+    header.querySelector(".heading").style = "opacity: 0;"
+    header.querySelector(".logo").style = "opacity: 0;"
+    sect1.removeEventListener("mouseover", sizeUp)
+    sect1.removeEventListener("mouseout", sizeDown)
+    sect2.removeEventListener("mouseover", sizeUp)
+    sect2.removeEventListener("mouseout", sizeDown)
+    sect3.removeEventListener("mouseover", sizeUp)
+    sect3.removeEventListener("mouseout", sizeDown)
+    sect4.removeEventListener("mouseover", sizeUp)
+    sect4.removeEventListener("mouseout", sizeDown)
     setTimeout(function(){
         sect2.querySelector(".bg").classList.remove("bg-active")
         sect2.querySelector(".content").classList.remove("bg-active")
         header.classList.remove("sect-active")
-    }, 8000)
+        header.querySelector(".logo-small").style = "opacity: 0;"
+        header.querySelector(".heading").style = "opacity: 1;"
+        header.querySelector(".logo").style = "opacity: 1;"
+        header.style = "clip-path: circle(114% at 50% 50%);"
+        sect1.addEventListener("mouseover", sizeUp)
+        sect1.addEventListener("mouseout", sizeDown)
+        sect2.addEventListener("mouseover", sizeUp)
+        sect2.addEventListener("mouseout", sizeDown)
+        sect3.addEventListener("mouseover", sizeUp)
+        sect3.addEventListener("mouseout", sizeDown)
+        sect4.addEventListener("mouseover", sizeUp)
+        sect4.addEventListener("mouseout", sizeDown)
+    }, 6000)
 })
 
 sect3.addEventListener("click", function(){
     sect3.querySelector(".bg").classList.add("bg-active")
     sect3.querySelector(".content").classList.add("bg-active")
     header.classList.add("sect-active")
+    header.querySelector(".logo-small").style = "opacity: 1;"
+    header.querySelector(".heading").style = "opacity: 0;"
+    header.querySelector(".logo").style = "opacity: 0;"
+    sect1.removeEventListener("mouseover", sizeUp)
+    sect1.removeEventListener("mouseout", sizeDown)
+    sect2.removeEventListener("mouseover", sizeUp)
+    sect2.removeEventListener("mouseout", sizeDown)
+    sect3.removeEventListener("mouseover", sizeUp)
+    sect3.removeEventListener("mouseout", sizeDown)
+    sect4.removeEventListener("mouseover", sizeUp)
+    sect4.removeEventListener("mouseout", sizeDown)
     setTimeout(function(){
         sect3.querySelector(".bg").classList.remove("bg-active")
         sect3.querySelector(".content").classList.remove("bg-active")
         header.classList.remove("sect-active")
-    }, 17000)
+        header.querySelector(".logo-small").style = "opacity: 0;"
+        header.querySelector(".heading").style = "opacity: 1;"
+        header.querySelector(".logo").style = "opacity: 1;"
+        header.style = "clip-path: circle(114% at 50% 50%);"
+        sect1.addEventListener("mouseover", sizeUp)
+        sect1.addEventListener("mouseout", sizeDown)
+        sect2.addEventListener("mouseover", sizeUp)
+        sect2.addEventListener("mouseout", sizeDown)
+        sect3.addEventListener("mouseover", sizeUp)
+        sect3.addEventListener("mouseout", sizeDown)
+        sect4.addEventListener("mouseover", sizeUp)
+        sect4.addEventListener("mouseout", sizeDown)
+    }, 6000)
 })
 
 sect4.addEventListener("click", function(){
     sect4.querySelector(".bg").classList.add("bg-active")
     sect4.querySelector(".content").classList.add("bg-active")
     header.classList.add("sect-active")
+    header.querySelector(".logo-small").style = "opacity: 1;"
+    header.querySelector(".heading").style = "opacity: 0;"
+    header.querySelector(".logo").style = "opacity: 0;"
+    sect1.removeEventListener("mouseover", sizeUp)
+    sect1.removeEventListener("mouseout", sizeDown)
+    sect2.removeEventListener("mouseover", sizeUp)
+    sect2.removeEventListener("mouseout", sizeDown)
+    sect3.removeEventListener("mouseover", sizeUp)
+    sect3.removeEventListener("mouseout", sizeDown)
+    sect4.removeEventListener("mouseover", sizeUp)
+    sect4.removeEventListener("mouseout", sizeDown)
     setTimeout(function(){
         sect4.querySelector(".bg").classList.remove("bg-active")
         sect4.querySelector(".content").classList.remove("bg-active")
         header.classList.remove("sect-active")
-    }, 4000)
+        header.querySelector(".logo-small").style = "opacity: 0;"
+        header.querySelector(".heading").style = "opacity: 1;"
+        header.querySelector(".logo").style = "opacity: 1;"
+        header.style = "clip-path: circle(114% at 50% 50%);"
+        sect1.addEventListener("mouseover", sizeUp)
+        sect1.addEventListener("mouseout", sizeDown)
+        sect2.addEventListener("mouseover", sizeUp)
+        sect2.addEventListener("mouseout", sizeDown)
+        sect3.addEventListener("mouseover", sizeUp)
+        sect3.addEventListener("mouseout", sizeDown)
+        sect4.addEventListener("mouseover", sizeUp)
+        sect4.addEventListener("mouseout", sizeDown)
+    }, 6000)
 })
 
 
